4ef4cae916688c7ac2e1f28833515df3f7765da4
Felipe Santiago <devfelipesantiago@gmail.com>
2025-02-16 23:05:55 -0300

    Fix: add response status on not found exception

--- minsait/src/main/java/com/br/minsait/controllers/dtos/contactDto.java
+++
@@ -1,7 +0,0 @@
-package com.br.minsait.controllers.dtos;
-
-import com.br.minsait.models.entities.ContactType;
-
-public record contactDto(ContactType contactType, String contact, int Person) {
-
-}
