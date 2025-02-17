370dae3e3f50d2e0eebe9607f0fe439f609edf45
Felipe Santiago <devfelipesantiago@gmail.com>
2025-02-17 13:24:11 -0300

    Feat: Test person service/create person

---
+++ minsait/src/test/java/com/br/minsait/services/PersonServiceTest.java
@@ -0,0 +1,70 @@
+package com.br.minsait.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+import com.br.minsait.models.entities.Person;
+import com.br.minsait.models.repositories.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+class PersonServiceTest {
+
+  @Autowired
+  PersonService personService;
+
+  @MockBean
+  PersonRepository personRepository;
+
+  @Test
+  void createPerson() {
+    Person person = new Person();
+    person.setName("Jonh Doe");
+    person.setAddress("123 Main St");
+    person.setZipCode("12345-678");
+    person.setCity("São Paulo");
+    person.setState("SP");
+
+    Person personToReturn = new Person();
+    personToReturn.setId(123);
+    personToReturn.setName(person.getName());
+    personToReturn.setAddress(person.getAddress());
+    personToReturn.setZipCode(person.getZipCode());
+    personToReturn.setCity(person.getCity());
+    personToReturn.setState(person.getState());
+
+    Mockito.when(personRepository.save(any())).thenReturn(personToReturn);
+
+    Person savedPerson = personService.createPerson(person);
+
+    Mockito.verify(personRepository).save(any());
+
+    assertEquals(123, savedPerson.getId());
+    assertEquals(person.getName(), savedPerson.getName());
+    assertEquals(person.getAddress(), savedPerson.getAddress());
+    assertEquals(person.getZipCode(), savedPerson.getZipCode());
+    assertEquals(person.getCity(), savedPerson.getCity());
+    assertEquals(person.getState(), savedPerson.getState());
+
+  }
+
+  @Test
+  void getAllPerson() {
+  }
+
+  @Test
+  void getPersonById() {
+  }
+
+  @Test
+  void updatePersonById() {
+  }
+
+  @Test
+  void deletePersonById() {
+  }
+}
