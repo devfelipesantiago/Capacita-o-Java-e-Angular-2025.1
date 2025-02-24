6e39a7bad99b9ba9f6c3accb6c1e6f6c95c62f81
Felipe Santiago <devfelipesantiago@gmail.com>
2025-02-24 19:01:23 -0300

    Feat: component people list

--- gerenciamento-de-contatos/src/app/components/people-list/people-list.component.ts
+++ gerenciamento-de-contatos/src/app/components/people-list/people-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Person } from '../../models/person';
+import { PersonService } from '../../services/person.service';
 
 @Component({
   selector: 'app-people-list',
   templateUrl: './people-list.component.html',
-  styleUrls: ['./people-list.component.css']
+  styleUrls: ['./people-list.component.css'],
 })
-export class PeopleListComponent {
+export class PeopleListComponent implements OnInit {
+  people: Person[] = [];
 
+  constructor(private personService: PersonService, private router: Router) {}
+
+  async ngOnInit() {
+    this.people = await this.personService.getAllPersons();
+  }
+
+  onEdit(id: number) {
+    this.router.navigate(['/edit', id]);
+  }
+
+  async onDelete(id: number) {
+    if (confirm('Are you sure you want to delete this person?')) {
+      await this.personService.deletePerson(id);
+      this.people = this.people.filter((person) => person.id !== id);
+    }
+  }
 }
