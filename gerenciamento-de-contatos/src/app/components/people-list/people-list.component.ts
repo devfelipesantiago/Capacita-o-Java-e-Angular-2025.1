eb0646a509de7a9d56b8c928c8e58b0ce30feb1b
Felipe Santiago <devfelipesantiago@gmail.com>
2025-02-26 11:50:47 -0300

    Feat: create component list with all people in database

--- gerenciamento-de-contatos/src/app/components/people-list/people-list.component.ts
+++ gerenciamento-de-contatos/src/app/components/people-list/people-list.component.ts
@@ -13,18 +13,42 @@ export class PeopleListComponent implements OnInit {
 
   constructor(private personService: PersonService, private router: Router) {}
 
-  async ngOnInit() {
-    this.people = await this.personService.getAllPersons();
+  ngOnInit(): void {
+    this.loadPeople();
   }
 
-  onEdit(id: number) {
-    this.router.navigate(['/edit', id]);
-  }
-
-  async onDelete(id: number) {
-    if (confirm('Are you sure you want to delete this person?')) {
-      await this.personService.deletePerson(id);
-      this.people = this.people.filter((person) => person.id !== id);
+  async loadPeople(): Promise<void> {
+    try {
+      this.people = await this.personService.getAllPersons();
+    } catch (error) {
+      console.error('Erro ao carregar pessoas:', error);
     }
+  }
+
+  getPhoneContacts(person: Person): string {
+    return person.contactList
+      .filter((contact) => contact.contactType === 'PHONE')
+      .map((contact) => contact.contact)
+      .join(', ');
+  }
+
+  async deletePerson(id: number) {
+    if (confirm('Tem certeza que deseja excluir esta pessoa?')) {
+      try {
+        await this.personService.deletePerson(id);
+        this.people = this.people.filter((p) => p.id !== id);
+      } catch (error) {
+        console.error('Erro ao excluir pessoa:', error);
+        alert('Erro ao excluir pessoa. Por favor, tente novamente.');
+      }
+    }
+  }
+
+  editPerson(id: number) {
+    this.router.navigate(['/edit-person', id]);
+  }
+
+  addNewPerson(): void {
+    this.router.navigate(['/add-person']);
   }
 }
