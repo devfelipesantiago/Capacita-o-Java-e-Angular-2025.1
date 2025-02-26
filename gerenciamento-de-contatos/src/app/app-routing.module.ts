3482dad5290f24a7b88cc5edc0f4ef30e1c61257
Felipe Santiago <devfelipesantiago@gmail.com>
2025-02-26 11:52:32 -0300

    Feat: update routing

--- gerenciamento-de-contatos/src/app/app-routing.module.ts
+++ gerenciamento-de-contatos/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PersonFormComponent } from './components/person-form/person-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PeopleListComponent },
+  { path: 'add-person', component: PersonFormComponent },
+  { path: 'edit-person/:id', component: PersonFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
