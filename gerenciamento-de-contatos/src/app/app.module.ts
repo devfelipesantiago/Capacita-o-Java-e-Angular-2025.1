07150a503d4a9c2077946beab90b1aa7c312b276
Felipe Santiago <devfelipesantiago@gmail.com>
2025-02-26 11:54:01 -0300

    Fix: Modules app

--- gerenciamento-de-contatos/src/app/app.module.ts
+++ gerenciamento-de-contatos/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,13 +14,15 @@ import { PersonFormComponent } from './components/person-form/person-form.compon
     AppComponent,
     PeopleListComponent,
     PersonTableComponent,
-    PersonFormComponent
+    PersonFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
